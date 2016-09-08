3b8365da8cab56ded9a3ae9ba86dc40fde982ae5
Hasanul Banna <banna@Banna-Macbook-Pro.local>
2016-09-08 12:58:08 +0530

    Ready set go..

---
+++ pay.php
@@ -0,0 +1,40 @@
+<?php 
+$product_name = $_POST["product_name"];
+$price = $_POST["product_price"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+
+include 'src/instamojo.php';
+
+$api = new Instamojo\Instamojo('a0143f32016b5cedac29f1d5d7945e06', 'ccc22327f73bfd4b1ce68af9dfb2f287','https://test.instamojo.com/api/1.1/');
+
+
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $product_name,
+        "amount" => $price,
+        "buyer_name" => $name,
+        "phone" => $phone,
+        "send_email" => true,
+        "send_sms" => true,
+        "email" => $email,
+        'allow_repeated_payments' => false,
+        "redirect_url" => "http://demo.coregenie.com/instamojo/thankyou.php",
+        "webhook" => "http://demo.coregenie.com/instamojo/webhook.php"
+        ));
+    //print_r($response);
+
+    $pay_ulr = $response['longurl'];
+    
+    //Redirect($response['longurl'],302); //Go to Payment page
+
+    header("Location: $pay_ulr");
+    exit();
+
+}
+catch (Exception $e) {
+    print('Error: ' . $e->getMessage());
+}     
+  ?>
